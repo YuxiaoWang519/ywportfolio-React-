832b8a12e8b2e446f118afec80bdf47796f3fc08
YuxiaoWang519 <84213964+YuxiaoWang519@users.noreply.github.com>
2022-05-10 05:29:08 -0400

    Update SideMenu.js

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,14 +4,27 @@ import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import "../styles/SideMenu.css";
+import Drawer from "@mui/material/Drawer";
+import SpeedDial from "@mui/material/SpeedDial";
+
 function SideMenu() {
+  const [nav, setNav] = React.useState(false);
+  function toggleNav() {
+    setNav(() => !nav);
+    console.log(nav);
+  }
   return (
     <div className="NavFBtn">
-      <Fab variant="extended">
+      <Fab onClick={toggleNav} variant="extended">
         <NavigationIcon sx={{ mr: 1 }} />
-        Navigate
+        Navigate<ArrowForwardIosIcon></ArrowForwardIosIcon>
       </Fab>
-      <ArrowForwardIosIcon></ArrowForwardIosIcon>
+
+      {/* use drawer to show nav */}
+      {/* <Drawer anchor="left" open={nav} onClose={toggleNav}>
+        <div>this is Drawer</div>
+      </Drawer> */}
+      {/* use speeddial to show nav */}
     </div>
   );
 }
