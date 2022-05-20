92f6f33616ef49965e20568df154899d9242fee7
YuxiaoWang519 <84213964+YuxiaoWang519@users.noreply.github.com>
2022-05-20 03:59:04 -0400

    Update AboutMe.js

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -1,12 +1,42 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import SideMenu from "../components/SideMenu";
+import { Toolbar } from "@mui/material";
+import YWFooter from "./YWFooter";
 
 function AboutMe() {
   return (
     <>
-      <Container>
-        <Box>this is about me page , do it later</Box>
+      <Container maxWidth="false" disableGutters={true} sx={{ width: "100%" }}>
+        <Box>
+          {/* <ElevationScroll {...props}> */}
+          <SideMenu></SideMenu>
+          {/* </ElevationScroll> */}
+          <Toolbar disableGutters={true} />
+        </Box>
+      </Container>
+      <Container
+        disableGutters
+        maxWidth={false}
+        sx={{ mx: "0", width: "100vw", background: "grey" }}
+      >
+        <Box>
+          <Typography>
+            Hi, I am Yuxiao. I'm a front end developer believed in simplicity
+            and efficiency.
+          </Typography>
+        </Box>
+      </Container>
+      <Container
+        id="contactAnchor"
+        maxWidth="false"
+        disableGutters={true}
+        sx={{ mt: "20vh", width: "100%", height: "20vh" }}
+      >
+        <Box sx={{ m: "5%" }}>
+          <YWFooter></YWFooter>
+        </Box>
       </Container>
     </>
   );
